d9acb05850331d17f4680a618c5b4c821d4d305e
syeremy <syeremy@gmail.com>
2018-04-30 03:56:05 -0400

    update dotnet-core  sdk and runtime to 2.1-preview2

--- vanlightly/source/registry/Govrnz.Registry.WebApi/Program.cs
+++ vanlightly/source/registry/Govrnz.Registry.WebApi/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace ElCavernas.Govrnz.Registry.WebApi
+namespace Elcavernas.Govrnz.Registry.WebApi
 {
     public class Program
     {
--- vanlightly/source/registry/Govrnz.Registry.WebApi/Startup.cs
+++ vanlightly/source/registry/Govrnz.Registry.WebApi/Startup.cs
@@ -5,12 +5,22 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ElCavernas.Govrnz.Registry.WebApi
+namespace Elcavernas.Govrnz.Registry.WebApi
 {
     public class Startup
     {
+        private IConfiguration _configuration;
+        
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
